43656fae7f48ac87ed8189ab70f8503daad7e2a2
VWRoli <fustroli@gmail.com>
2021-03-26 15:25:41 +0100

    added about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,100 @@
 const About = () => {
   return (
-    <div>
-      <h1>About page</h1>
-    </div>
+    <section className="about">
+      <h1>About Us</h1>
+      <h2>Best place to find the best gears</h2>
+      <img
+        src="https://images.unsplash.com/photo-1540281772389-9c107ba8c7c9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80"
+        alt="Guitar"
+        className="about-img1"
+      />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure culpa
+        recusandae consequatur ea distinctio eius quas neque, saepe amet est
+        repudiandae excepturi in sapiente magnam dignissimos consequuntur rem
+        sint at numquam molestiae dicta id repellendus animi maxime? Aut ab
+        cumque adipisci, illum ipsum, quam rem laboriosam illo sit quas vero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure culpa
+        recusandae consequatur ea distinctio eius quas neque, saepe amet est
+        repudiandae excepturi in sapiente magnam dignissimos consequuntur rem
+        sint at numquam molestiae dicta id repellendus animi maxime? Aut ab
+        cumque adipisci, illum ipsum, quam rem laboriosam illo sit quas vero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam commodi
+        mollitia et totam eligendi dolorem doloremque necessitatibus ipsum sint
+        culpa neque recusandae asperiores expedita cumque, exercitationem
+        numquam enim dolores reiciendis nam debitis. Eos, delectus praesentium?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure culpa
+        recusandae consequatur ea distinctio eius quas neque, saepe amet est
+        repudiandae excepturi in sapiente magnam dignissimos consequuntur rem
+        sint at numquam molestiae dicta id repellendus animi maxime? Aut ab
+        cumque adipisci, illum ipsum, quam rem laboriosam illo sit quas vero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure culpa
+        recusandae consequatur ea distinctio eius quas neque, saepe amet est
+        repudiandae excepturi in sapiente magnam dignissimos consequuntur rem
+        sint at numquam molestiae dicta id repellendus animi maxime? Aut ab
+        cumque adipisci, illum ipsum, quam rem laboriosam illo sit quas vero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure culpa
+        recusandae consequatur ea distinctio eius quas neque, saepe amet est
+        repudiandae excepturi in sapiente magnam dignissimos consequuntur rem
+        sint at numquam molestiae dicta id repellendus animi maxime? Aut ab
+        cumque adipisci, illum ipsum, quam rem laboriosam illo sit quas vero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam commodi
+        mollitia et totam eligendi dolorem doloremque necessitatibus ipsum sint
+        culpa neque recusandae asperiores expedita cumque, exercitationem
+        numquam enim dolores reiciendis nam debitis. Eos, delectus praesentium?
+      </p>
+      <img
+        src="https://images.unsplash.com/photo-1593419543845-a6748cf68b1e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
+        alt="Guitar"
+        className="about-img2"
+      />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure culpa
+        recusandae consequatur ea distinctio eius quas neque, saepe amet est
+        repudiandae excepturi in sapiente magnam dignissimos consequuntur rem
+        sint at numquam molestiae dicta id repellendus animi maxime? Aut ab
+        cumque adipisci, illum ipsum, quam rem laboriosam illo sit quas vero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure culpa
+        recusandae consequatur ea distinctio eius quas neque, saepe amet est
+        repudiandae excepturi in sapiente magnam dignissimos consequuntur rem
+        sint at numquam molestiae dicta id repellendus animi maxime? Aut ab
+        cumque adipisci, illum ipsum, quam rem laboriosam illo sit quas vero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure culpa
+        recusandae consequatur ea distinctio eius quas neque, saepe amet est
+        repudiandae excepturi in sapiente magnam dignissimos consequuntur rem
+        sint at numquam molestiae dicta id repellendus animi maxime? Aut ab
+        cumque adipisci, illum ipsum, quam rem laboriosam illo sit quas vero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi aperiam
+        saepe hic aut rem possimus consequatur odit nulla iste nihil expedita,
+        optio inventore sed! Vero est sequi quaerat reprehenderit alias aperiam
+        maiores at, excepturi consectetur fugit ullam velit harum laudantium?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure culpa
+        recusandae consequatur ea distinctio eius quas neque, saepe amet est
+        repudiandae excepturi in sapiente magnam dignissimos consequuntur rem
+        sint at numquam molestiae dicta id repellendus animi maxime? Aut ab
+        cumque adipisci, illum ipsum, quam rem laboriosam illo sit quas vero.
+      </p>
+    </section>
   );
 };
 
