1281ff89daefe9797c9e7107e1f91d69c5dd1e6c
VWRoli <fustroli@gmail.com>
2021-03-29 10:23:27 +0200

    added onsale page

--- src/components/OnSale.js
+++ src/components/OnSale.js
@@ -1,9 +1,41 @@
-const OnSale = () => {
+import { filterProducts } from '../helpers';
+import { useFetch } from '../useFetch';
+import ListItem from './ListItem';
+import Loading from '../Loading';
+import Error from '../Error';
+import { API_ROOT } from '../constant';
+
+const ProductsList = () => {
+  const { data: products, isLoading, isError } = useFetch(API_ROOT);
+  //Loading screen
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  //Error Message
+  if (isError) {
+    return <Error />;
+  }
+
+  //Get on sale guitars
+  const guitarsOnSale = filterProducts(products, 'guitar', 'isOnSale');
+  const ampsOnSale = filterProducts(products, 'amp', 'isOnSale');
+  const productsOnSale = [...guitarsOnSale, ...ampsOnSale];
+
   return (
-    <div>
-      <h1>On sale products</h1>
-    </div>
+    <>
+      <h1 className="onsale-title">Our On Sale Products:</h1>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="onsale-wrapper">
+          {productsOnSale.map((product) => (
+            <ListItem product={product} key={product.id} />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
-export default OnSale;
+export default ProductsList;
