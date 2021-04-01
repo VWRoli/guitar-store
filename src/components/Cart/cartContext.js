1e3222e9c5c600dfc74f74d19d9cdea757ffe6cc
VWRoli <fustroli@gmail.com>
2021-04-01 10:28:20 +0200

    added local storage

--- src/components/Cart/cartContext.js
+++ src/components/Cart/cartContext.js
@@ -17,12 +17,25 @@ const initialState = {
   compare: [],
   total: 0,
   amount: 0,
-  compareAmount: 0,
   isMessageOpen: false,
 };
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initialState);
+
+  //Get LocalStorage
+  useEffect(() => {
+    //get cart data
+    const cartData = localStorage.getItem('cartData');
+    const savedCartData = JSON.parse(cartData);
+    savedCartData ? (state.cart = savedCartData) : (state.cart = []);
+    //get cart data
+    const compareData = localStorage.getItem('compareData');
+    const savedCompareData = JSON.parse(compareData);
+    savedCompareData
+      ? (state.compare = savedCompareData)
+      : (state.compare = []);
+  }, []);
 
   const clearItems = (source) => {
     dispatch({ type: CLEAR_CART, payload: source });
@@ -52,6 +65,16 @@ export const CartProvider = ({ children }) => {
   const closeMessage = () => {
     dispatch({ type: CLOSE_MESSAGE });
   };
+
+  //Set LocalStorage
+  useEffect(() => {
+    //set cart data
+    const cartData = JSON.stringify(state.cart);
+    localStorage.setItem('cartData', cartData);
+    //set compare data
+    const compareData = JSON.stringify(state.compare);
+    localStorage.setItem('compareData', compareData);
+  }, [state.cart, state.compare]);
 
   return (
     <CartContext.Provider
